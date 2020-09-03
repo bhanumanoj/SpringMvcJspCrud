b31f76979ec971d974d4b50d1f5aec50f2093631
bhanumanoj <31289904+bhanumanoj@users.noreply.github.com>
2020-09-03 20:18:44 +0530

    Add files via upload

---
+++ SpringJspNext/src/main/java/com/capgemini/controller/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.capgemini.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.capgemini")
+public class MvcConfig extends WebMvcConfigurerAdapter{
+	
+	/*
+	 * @Bean public InternalResourceViewResolver viewResolver() {
+	 * InternalResourceViewResolver vr=new InternalResourceViewResolver();
+	 * vr.setPrefix("/webapp/"); vr.setSuffix(".jsp"); return vr;
+	 * 
+	 * }
+	 * 
+	 */}
